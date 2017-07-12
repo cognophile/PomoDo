67dc76caf4a830c968181d2d712c4be034f55b33
Cognophile <cognophile@users.noreply.github.com>
2017-07-12 13:10:13 +0100

    Initial project skeleton

---
+++ PomoDo/Tests/ExampleTest.cs
@@ -0,0 +1,19 @@
+﻿namespace NUnit.Tests
+{
+    using System;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class ExampleTest
+    {
+        [Test]
+        public void exampleTest_firstTestCaseTest()
+        {
+        }
+
+        [Test]
+        public void exampleTest_secondTestCaseTest()
+        {
+        }
+    }
+}
